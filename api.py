dfb5d5f7cd268d441a7dcd507b64f6f73ee67d06
Hunter Kamerman <devkamerman@gmail.com>
2023-08-01 09:37:41 -0400

    rewrote with aiohttp

--- api.py
+++
@@ -1,5 +0,0 @@
-import asyncio
-
-async def   fetch_data() -> str:
-    print ('fetching data...')
-    await asyncio.sleep(2.5)
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import os
 from dotenv import load_dotenv
 import discord
-import requests
-import asyncio
-load_dotenv
+import aiohttp
 
 API_URL = "https://meme-api.com/gimme"
 
-
+# Load environment variables from .env file
+load_dotenv()
 
 # Initialize the bot
 intents = discord.Intents.default()
 intents.messages = True
 client = discord.Client(intents=intents)
+
+# Create an aiohttp session to reuse connections
+session = aiohttp.ClientSession()
 
 
 # Event: on_ready
@@ -28,12 +30,12 @@ async def on_message(message):
         return
 
     if message.content.startswith("!meme"):
-        # Fetch meme from the API
+        # Fetch meme from the API using aiohttp
         try:
-            response = requests.get(API_URL)
-            data = response.json()
-            meme_url = data["url"]
-            await message.channel.send(meme_url)
+            async with session.get(API_URL) as response:
+                data = await response.json()
+                meme_url = data["url"]
+                await message.channel.send(meme_url)
         except Exception as e:
             await message.channel.send("Sorry, couldn't fetch a meme at the moment.")
 
@@ -43,3 +45,9 @@ bot_token = os.environ.get("BOT_TOKEN")
 
 # Run the bot
 client.run(bot_token)
+
+# Close the aiohttp session when the bot is stopped
+async def on_shutdown():
+    await session.close()
+
+client.loop.run_until_complete(on_shutdown())
